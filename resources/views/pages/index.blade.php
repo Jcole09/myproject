0a23a77cc863c4225ee239ab1ec172386ba4573e
Jcole09 <jcolereyes09@gmail.com>
2021-11-22 17:00:56 +0800

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,4 @@
+@section ('content')
+    <h1>Welcome Page</h1>
+    <p>This is Home Page</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+Route::get('/test/testpage', function () {
+    return view('test');
+});
+
+
+
+Route::get('ID/{id}', function ($id) {
+    return 'ID: ' .$id;
+});
+
+Route::get('ID/{id?}', function($id = null){
+return $id;
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+*/
+
+
+Route::get('/', 'MyController@index');
+Route::get('/about', 'MyController@about');
+Route::get('/service', 'MyController@services');
+
+Route::resource('post', 'PostController');
+
+Route::resource('post', 'RegistrationController');
